c034ba6dbcd765c96e3ade6ecdbca485233d14e4
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-10-30 10:01:00 +0700

    Add recoil and replace `App` to `Routes` component

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider } from "styled-components";
 import theme from "styles/theme";
+import { RecoilRoot } from "recoil";
+
+import Routes from "routes";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <RecoilRoot>
+      <ThemeProvider theme={theme}>
+        <Routes />
+      </ThemeProvider>
+    </RecoilRoot>
   </React.StrictMode>,
   document.getElementById("root")
 );
