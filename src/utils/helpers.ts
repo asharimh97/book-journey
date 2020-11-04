1195d6de38f39b8491159d06328f22b3fcf23715
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-11-04 21:35:32 +0700

    Add handle error image helper

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -3,3 +3,8 @@ export const convertNumberToPixel = (number: any) => {
 
   return `${number}px`;
 };
+
+export const handleErrorImage = (e: any) => {
+  e.target.onerror = null;
+  e.target.src = "/image-placeholder.jpg";
+};
