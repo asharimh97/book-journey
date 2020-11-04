82bb3c8a04964b4b19baffba9b2fc3af36191aaf
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-11-04 21:38:48 +0700

    Add Container

---
+++ src/components/atoms/Container.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import Box from "./Box";
+
+const Container = styled(Box)`
+  margin: auto;
+  width: 100%;
+  max-width: 920px;
+`;
+
+Container.displayName = "Container";
+
+export default Container;
--- src/components/atoms/index.tsx
+++ src/components/atoms/index.tsx
@@ -1,6 +1,7 @@
 export { default as Box } from "./Box";
 export { default as Button } from "./Button";
 export { default as Card } from "./Card";
+export { default as Container } from "./Container";
 export { default as Flex } from "./Flex";
 export { default as Grid } from "./Grid";
 export { default as Image } from "./Image";
