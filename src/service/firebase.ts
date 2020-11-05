6aed083c25ed53beceae34a89be480be2e1c5350
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-11-05 21:27:12 +0700

    Update firebase configs

--- src/service/firebase.ts
+++ src/service/firebase.ts
@@ -1,19 +1,19 @@
 import firebase from "firebase";
 
 export const config: Object = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
-
-export const auth = firebase.auth;
-export const db = firebase.database();
 
 // Initialize Firebase
 firebase.initializeApp(config);
 firebase.analytics();
+
+export const auth = firebase.auth;
+export const db = firebase.firestore();
