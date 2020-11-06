125791e5aa85cadadbcbec4e80471bc14c40badf
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-11-06 19:42:33 +0700

    Add missing styled system grid prop

--- src/components/atoms/Grid.tsx
+++ src/components/atoms/Grid.tsx
@@ -1,11 +1,12 @@
 import styled from "styled-components";
-import { GridProps } from "styled-system";
+import { grid, GridProps } from "styled-system";
 import Base, { BaseProp } from "./Base";
 
 type GridProp = BaseProp & GridProps;
 
 const Grid = styled(Base)<GridProp>`
   display: grid;
+  ${grid}
 `;
 
 Grid.displayName = "Grid";
