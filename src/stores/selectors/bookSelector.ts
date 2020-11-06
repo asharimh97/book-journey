d432a21e81cf6334f8e52b89f358898045096cb4
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-11-06 19:42:20 +0700

    Add recoil book selector

---
+++ src/stores/selectors/bookSelector.ts
@@ -0,0 +1,26 @@
+import { selector } from "recoil";
+import { db } from "service/firebase";
+import { booksState } from "stores/states";
+
+export const booksQuery = selector({
+  key: "booksCollection",
+  get: async ({ get }) => {
+    const res: Array<any> = get(booksState);
+
+    const bookRef = db.collection("books");
+    const snapshot = await bookRef.get();
+
+    if (snapshot.empty) return res;
+
+    snapshot.forEach(book => {
+      const bookData = {
+        ...book.data(),
+        id: book.id,
+        read_at: book.data().read_at.seconds
+      };
+      res.push(bookData);
+    });
+
+    return res;
+  }
+});
