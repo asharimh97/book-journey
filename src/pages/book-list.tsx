74ddc15fbad167320457a8f3c93277101bb8b4f4
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-11-06 19:49:16 +0700

    Add book list page

--- src/pages/book-list.tsx
+++ src/pages/book-list.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+import { Grid } from "components/atoms";
+import Layout from "components/Layout";
+import BookCard from "components/molecules/BookCard";
+import PageHeading from "components/molecules/PageHeading";
+import React, { useEffect } from "react";
+import { useRecoilValue } from "recoil";
+import { booksQuery } from "stores/selectors";
 
 const BookList: React.FC = () => {
-  return <div>Ini page Book list</div>;
+  const books = useRecoilValue(booksQuery);
+
+  useEffect(() => {
+    console.log(books);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <Layout>
+      <PageHeading
+        data-testid="page-heading"
+        title="Books I've read"
+        subtitle="Explore"
+      />
+      <Grid
+        data-testid="book-list"
+        gridTemplateColumns={[
+          "repeat(2, 1fr)",
+          null,
+          "repeat(4, 1fr)",
+          "repeat(6, 1fr)"
+        ]}
+        gridGap={20}
+      >
+        {books.map(book => (
+          <BookCard
+            data-testid="book-item"
+            book={book}
+            minWidth={136}
+            maxWidth={158}
+          />
+        ))}
+      </Grid>
+    </Layout>
+  );
 };
 
 export default BookList;
