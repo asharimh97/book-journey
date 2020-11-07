7f0407e082c21db65a5a9420e6e3d3dd4fa1d16d
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-11-07 21:47:49 +0700

    Add link to navbar logo

--- src/components/molecules/Navbar.tsx
+++ src/components/molecules/Navbar.tsx
@@ -1,3 +1,4 @@
+import { Link } from "@reach/router";
 import { Box, Container, Image } from "components/atoms";
 import React from "react";
 
@@ -5,7 +6,9 @@ const Navbar: React.FC = () => {
   return (
     <Box display={["none", null, "block"]}>
       <Container py={4}>
-        <Image src="/logo.svg" width={150} />
+        <Link to="/">
+          <Image src="/logo.svg" width={150} />
+        </Link>
       </Container>
     </Box>
   );
