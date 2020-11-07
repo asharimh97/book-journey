e9ac0dbec657580dc5e601c58a81490c0fd2a46e
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-11-07 20:50:01 +0700

    Add dynamic title in layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,19 @@
-import React, { useRef } from "react";
+import React from "react";
+import Helmet from "react-helmet";
 import { Container } from "./atoms";
 import Navbar from "./molecules/Navbar";
 
 type Prop = {
   children?: any;
+  title?: string;
 };
 
-const Layout: React.FC<Prop> = ({ children, ...props }) => {
+const Layout: React.FC<Prop> = ({ children, title, ...props }) => {
   return (
     <>
+      <Helmet>
+        <title>{title && `${title} - `}Book Journey</title>
+      </Helmet>
       <Navbar />
       <Container
         height={["100vh", null, "calc(100vh - 98px)"]}
