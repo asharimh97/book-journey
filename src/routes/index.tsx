53f95bcd649446dfdf18862a1314a6d2ff53e258
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-11-10 20:30:10 +0700

    Add page `add book` to routing

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,12 @@
 import { Router } from "@reach/router";
+import AddBook from "pages/book-add";
 import BookDetail from "pages/book-detail";
 import BookList from "pages/book-list";
 import Home from "pages/home";
 import Login from "pages/login";
+import Logout from "pages/logout";
 import React from "react";
+import PrivateRoute from "./PrivateRoute";
 import PublicRoute from "./PublicRoute";
 
 const Routes: React.FC = () => {
@@ -13,6 +16,9 @@ const Routes: React.FC = () => {
       <PublicRoute page={Login} path="/login" />
       <PublicRoute page={BookList} path="/books" />
       <PublicRoute page={BookDetail} path="/book/:id" />
+      <PublicRoute page={Logout} path="/logout" />
+
+      <PrivateRoute page={AddBook} path="/add-book" />
     </Router>
   );
 };
