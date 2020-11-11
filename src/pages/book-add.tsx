42e35e6b641b7a2019675bb0cd65468fcc35c251
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-11-11 20:21:15 +0700

    Add CardAddBook to page add book

--- src/pages/book-add.tsx
+++ src/pages/book-add.tsx
@@ -1,10 +1,15 @@
 import Layout from "components/Layout";
+import CardAddBook from "components/organisms/CardAddBook";
 import React from "react";
 
 const AddBook: React.FC = () => {
+  const handleAddBook = (data: any) => {
+    console.log(data);
+  };
+
   return (
     <Layout title="Add Book">
-      <div>Ini halaman add book</div>
+      <CardAddBook onSubmit={handleAddBook} />
     </Layout>
   );
 };
