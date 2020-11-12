1fc9eba8b4226104f446ae6fb357639dd73d7d80
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-11-12 20:20:19 +0700

    Add handler to add new book

--- src/pages/book-add.tsx
+++ src/pages/book-add.tsx
@@ -1,10 +1,41 @@
 import Layout from "components/Layout";
 import CardAddBook from "components/organisms/CardAddBook";
 import React from "react";
+import { db, storage } from "service/firebase";
 
 const AddBook: React.FC = () => {
-  const handleAddBook = (data: any) => {
-    console.log(data);
+  const handleUploadCover = async (image: File) => {
+    const storageRef = storage().ref();
+
+    try {
+      const imageName = `${Date.now()}-${image.name}`;
+      const imageRef = await storageRef.child(imageName).put(image);
+      const url = await imageRef.ref.getDownloadURL();
+
+      return url;
+    } catch (error) {
+      console.error(error);
+      return "";
+    }
+  };
+
+  const handleAddBook = async (val: any, e: any) => {
+    // construct data file
+    const { cover, ...rest } = val;
+    const data = {
+      ...rest
+    };
+
+    data.cover = await handleUploadCover(cover[0]);
+
+    // add book cover to firebase storage
+    const addBook = await db.collection("books").add(data);
+
+    // reset form if success
+    if (addBook.id) {
+      e.target.reset();
+      alert("Success add new book!");
+    }
   };
 
   return (
