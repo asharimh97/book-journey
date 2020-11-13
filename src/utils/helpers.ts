098de7b4cef183e3d58df735e425e83370dcf4a5
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-11-13 19:44:01 +0700

    Add get random number by number param

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -8,3 +8,7 @@ export const handleErrorImage = (e: any) => {
   e.target.onerror = null;
   e.target.src = "/image-placeholder.jpg";
 };
+
+export const generateRandomNumber = (range: number) => {
+  return Math.round(Math.random() * range);
+};
