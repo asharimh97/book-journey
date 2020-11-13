40d2b6bdbff128f5d0839201e4fceff4a758489d
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-11-13 19:43:51 +0700

    Add quotes constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,47 @@
+export const QUOTES = [
+  {
+    quotes: `So long as victory can be attained, stupid haste is preferable to
+            clever dilatoriness.`,
+    author: "Sun Tzu",
+    title: "The Art of War"
+  },
+  {
+    quotes: `If you’re serious about changing your life, you’ll find a way. 
+            If you’re not, you’ll find an excuse.`,
+    author: "Jen Sincero",
+    title: "You are a Badass"
+  },
+  {
+    quotes: `“You can love someone and still choose to say goodbye to them,” 
+            she says now. “You can miss a person every day, and still be 
+            glad that they are no longer in your life.”`,
+    author: "Tara Westover",
+    title: "Educated"
+  },
+  {
+    quotes: `Awake,arise,and assert yourself,you dreamers of the world. 
+            Your star is now in ascendancy.`,
+    author: "Napoleon Hill",
+    title: "Think and Grow Rich"
+  },
+  {
+    quotes: `This is the essence of intuitive heuristics: when faced 
+            with a difficult question, we often answer an easier one instead, 
+            usually without noticing the substitution.`,
+    author: "Daniel Kahneman",
+    title: "Thinking, Fast and Slow"
+  },
+  {
+    quotes: `If you are ready for the secret, you already possess one half
+            of it,therefore, you will readily recognize the other half 
+            the moment it reaches your mind.`,
+    author: "Napoleon Hill",
+    title: "Think and Grow Rich"
+  },
+  {
+    quotes: `The desert is a capricious lady, and sometimes she drives men 
+            crazy.`,
+    author: "Paulo Coelho",
+    title: "The Alchemist"
+  }
+];
