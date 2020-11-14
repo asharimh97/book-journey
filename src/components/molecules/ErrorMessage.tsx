5702703e750f65da21ffdd507aa2486449cae84f
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-11-14 20:22:25 +0700

    Add error message component

---
+++ src/components/molecules/ErrorMessage.tsx
@@ -0,0 +1,38 @@
+import { Box, Flex, Text, Title } from "components/atoms";
+import React from "react";
+
+type Props = {
+  code?: number;
+  message?: string;
+};
+
+const ErrorMessage: React.FC<Props> = ({ code, message }) => {
+  return (
+    <Flex
+      alignItems="center"
+      position="absolute"
+      top="50%"
+      left="50%"
+      style={{ transform: "translate(-50%, -50%)" }}
+    >
+      <Title as="h3" tight>
+        {code}
+      </Title>
+      <Box
+        width="2px"
+        minHeight={26}
+        height="100%"
+        backgroundColor="black.80"
+        mx={4}
+      />
+      <Text>{message}</Text>
+    </Flex>
+  );
+};
+
+ErrorMessage.defaultProps = {
+  code: 404,
+  message: "Oops! It seems you are lost buddy"
+};
+
+export default ErrorMessage;
