182c0a40e896dcd785b1685c26899215dbd38f5e
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-11-15 13:18:57 +0700

    Update cookie duration to 12 hours

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -2,9 +2,12 @@ import Cookie from "js-cookie";
 
 export const APP_COOKIE = "bookJourney";
 
+const HOUR_IN_SECOND = 3600;
+const HOUR_DURATION = 12;
+
 export const setCookie = (key: string, val: string | object) => {
   Cookie.set(key, val, {
-    expires: 7 * 24 * 3600
+    expires: HOUR_DURATION * HOUR_IN_SECOND
   });
 };
 
