d871abca523649c4d56d25b9f338f2b04bbb9c23
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-01-20 21:09:02 +0700

    Update helper

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,14 +1,16 @@
-export const convertNumberToPixel = (number: any) => {
+export const convertNumberToPixel = (number: number): string | number => {
   if (isNaN(number)) return number;
 
   return `${number}px`;
 };
 
-export const handleErrorImage = (e: any) => {
+export const handleErrorImage = (e: Event) => {
+  // @ts-ignore
   e.target.onerror = null;
+  // @ts-ignore
   e.target.src = "/image-placeholder.jpg";
 };
 
-export const generateRandomNumber = (range: number) => {
+export const generateRandomNumber = (range: number): number => {
   return Math.round(Math.random() * range);
 };
